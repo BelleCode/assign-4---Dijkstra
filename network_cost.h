fa36d1e04cc096ee99a57b419daf23dec4f902fe
Belinda Tiberio <belinda.tiberio@gmail.com>
2013-03-03 13:43:56 -0800

    Initial Commit

---
+++ network_cost.h
@@ -0,0 +1,21 @@
+//	****************************************************************************
+//	network_cost.h
+//
+//	Project: lab04
+// 	purpose: 
+//		read a cost table from cin and take two command line arguments for the 
+//		start and end cities
+//	goal:
+//		Computes the minimum-cost path and print to cout in proper order in the
+//		same format
+//	
+//
+//	****************************************************************************
+
+#ifndef NETWORK_COST_H
+#define NETWORK_COST_H
+
+#include <iostream>
+#include <string>
+
+class Network 
